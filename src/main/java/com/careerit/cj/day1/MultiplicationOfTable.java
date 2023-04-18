3fc873034cee1abce8818c397ae04f12de97f2f4
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-18 15:26:23 -0400

    LW001 print in 20 to 1

--- src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
+++ src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
@@ -3,8 +3,12 @@ package com.careerit.cj.day1;
 public class MultiplicationOfTable {
     public static void main(String[] args) {
         int num = 19;
-        for(int i=1;i <=10; i++){
-            System.out.println(String.format("%s * %s",num, i, num * i));
+        for(int i=1;i <=20; i++){
+            System.out.println(String.format("%s * %s = %s",num, i, num * i));
+        }
+        for (int i=20; i >=1; i--) {
+            System.out.println(String.format("%s * %s = %s",num, i, num * i));
+
         }
     }
 }
