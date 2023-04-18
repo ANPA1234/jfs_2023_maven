cf6a39f1c23e08195549b1de100339009ba6b6dc
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-18 11:55:05 -0400

    added extra log message and multiplication table added and changes factor

--- src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
+++ src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
@@ -4,6 +4,7 @@ public class MultiplicationOfTable {
     public static void main(String[] args) {
         int num = 19;
         for(int i=1;i <=10; i++){
-            System.out.println(String.format("%s * %s",num,i,num*i));        }
+            System.out.println(String.format("%s * %s",num, i, num * i));
+        }
     }
 }
