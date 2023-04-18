a45e7d9f2b2576c4bff669e3d7e5e9f8961827c7
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-18 11:50:25 -0400

    added extra log message and multiplication table added

--- src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
+++ src/main/java/com/careerit/cj/day1/MultiplicationOfTable.java
@@ -3,8 +3,7 @@ package com.careerit.cj.day1;
 public class MultiplicationOfTable {
     public static void main(String[] args) {
         int num = 19;
-        for(int i=1;i<=10;i++){
-            System.out.println(String.format("%s * %s = %s",num,i,num*i));
-        }
+        for(int i=1;i <=10; i++){
+            System.out.println(String.format("%s * %s",num,i,num*i));        }
     }
 }
